d379ccc015abaa83bdace2f433659cc2f9422dd7
Santosh Singh Pawar <textingsantosh@gmail.com>
2025-10-17 09:00:57 +0530

    project made

--- swiggy-clone/src/utils/firebase.js
+++ swiggy-clone/src/utils/firebase.js
@@ -1,19 +1,18 @@
-import dotenv from "dotenv";
+
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
-dotenv.config();
+import { getAuth } from "firebase/auth";
+
 
 const firebaseConfig = {
-  apiKey: process.env.Firebase_APIKey,
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
   authDomain: "tweeter-message.firebaseapp.com",
   projectId: "tweeter-message",
   storageBucket: "tweeter-message.firebasestorage.app",
   messagingSenderId: "539097243023",
-  appId: "1:539097243023:web:93aa84dbcd42efc8b3bffa"
+  appId: "1:539097243023:web:93aa84dbcd42efc8b3bffa",
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export {auth};
+export { auth };
